ac5c0d2972668cb6ddd2b16b7a3bbb5c42016c28
DoDi1408 <ro.chavez.5454@gmail.com>
2024-06-08 20:53:03 -0600

    what is going oooon

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from logging.config import dictConfig
 from flask import Flask, jsonify, request
 from openai import OpenAI
-import datetime
+from datetime import datetime, timedelta
 
 dictConfig({
     'version': 1,
@@ -31,13 +31,15 @@ def test():
 @app.route("/task", methods=['POST'])
 def get_task_JSON():
     app.logger.info('received a /task')
+    d = datetime.today() - timedelta(hours=6)
+    app.logger.info(str(d))
     task_message = request.get_data(as_text=True)
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-0125",
         temperature=0.3,
         response_format={ "type": "json_object" },
         messages=[
-            {"role": "system", "content": "The current time is: " + str(datetime.datetime.now())  + " but minus 6 hours"},
+            {"role": "system", "content": "The current time is: " + str(d)  + " but minus 6 hours"},
             {"role": "system", "content": "Do not include hour on startDate or dueDate, format must be yyyy-MM-dd"},
             {"role": "system", "content": "You will receive text in spanish, make sure you dont subtitute letters with ñ or accents"},
             {"role": "system", "content": "You are a helpful assistant designed to output JSON. IT MUST have the following attributes. startDate, dueDate, description, title, stateTask (this one is always 0), and employee (this one is always null)"},
